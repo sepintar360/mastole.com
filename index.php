250cab9c183744d64c632dacb489e4c2492d0e65
sepintar360 <61118412+sepintar360@users.noreply.github.com>
2021-03-16 17:07:33 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+function bacaHTML($url){
+     // inisialisasi CURL
+     $data = curl_init();
+     // setting CURL
+     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($data, CURLOPT_URL, $url);
+     // menjalankan CURL untuk membaca isi file 
+     $hasil = curl_exec($data);
+     curl_close($data);
+     return $hasil;
+}
+ 
+echo bacaHTML('https://www.mastole.com');
+ 
+?>
