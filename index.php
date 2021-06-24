b7718dc0f4f7b715f6dbff7e6d5fa16ae36272e6
sepintar360 <61118412+sepintar360@users.noreply.github.com>
2021-06-24 19:03:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-<?php
-function bacaHTML($url){
-     // inisialisasi CURL
-     $data = curl_init();
-     // setting CURL
-     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-     curl_setopt($data, CURLOPT_URL, $url);
-     // menjalankan CURL untuk membaca isi file 
-     $hasil = curl_exec($data);
-     curl_close($data);
-     return $hasil;
-}
- 
-echo bacaHTML('https://www.haruspinter.com');
- 
-?>
+
